571a8a90929c1ca471e074ebb063b7d213e2477b
ewarrenG <eglasenk@gmail.com>
2018-02-07 12:12:41 -0500

    Updated various assets

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 //included for the purposes of reading information from Box
+//part of initial dev work
 var box = new BoxSdk();
-var accessToken = "UnbOsuLrVLVpDv6NWdfe77xCjIrXRyQS";
+var accessToken = "FwHHAfeRnky1PlfzffURG5rugoB9sBza"; //expired
 var boxClient = new box.BasicBoxClient({ accessToken: accessToken });
 boxClient.folders.get({ id: "46199583831", params: {fields: "name,item_collection"} })
   .then(function (folder) {
