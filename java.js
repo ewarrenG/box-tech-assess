dd97d95b11ff1a7aa3a0fe8511b392816595bcf7
ewarrenG <eglasenk@gmail.com>
2018-02-10 18:26:06 -0500

    updated for content preview and metadata

--- java.js
+++ java.js
@@ -1,9 +1,26 @@
+//create userid
+function makeId(strLength) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < strLength; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+};
+var userId = makeId(32);
+window.location.hash = userId;
+
+$('body').append('<span class="abs-top-right">' + userId + '</span>');
+
+
+
 //included for the purposes of reading information from Box
 //part of initial dev work
 var box = new BoxSdk();
-var accessToken = "FwHHAfeRnky1PlfzffURG5rugoB9sBza"; //expired
+var accessToken = "Ym67uwveS5EhhUrho1GstzlkunIo4B4x"; //expired
 var boxClient = new box.BasicBoxClient({ accessToken: accessToken });
-boxClient.folders.get({ id: "46199583831", params: {fields: "name,item_collection"} })
+
+var folderId = '46199583831'; //loan applications
+boxClient.folders.get({ id: folderId, params: {fields: "name,item_collection"} })
   .then(function (folder) {
     var rootFolder = folder;
     var id = folder.id;
@@ -12,5 +29,56 @@ boxClient.folders.get({ id: "46199583831", params: {fields: "name,item_collectio
   .catch(function (err) {
     console.log(err);
   });
+
+
+//uploader
+var uploaderOptions = {
+container: '.container',
+size: 'small', 
+onClose: function(){
+  location.reload();
+}
+}
+var uploader = new Box.ContentUploader();
+uploader.on('complete', function(data){
+  //console.log('complete');
+  //console.log(data);
+  enablePreview(data[0].id);
+  attachMeta(data[0].id);
+  $(".preview-container").outerHeight(525);
+});
+uploader.show(folderId, accessToken, uploaderOptions);
+
+
+//preview
+var previewOptions = {
+  container: '.preview-container',
+  showDownload: true,
+  logoUrl: 'assets/preview.png'
+}
+var preview = new Box.Preview();
+function enablePreview(fileId){
+	preview.show(fileId, accessToken, previewOptions);
+	$('ul.nav-tabs li:last').removeClass("hide");
+	//$('li a[href="#preview"]').attr("data-toggle", "tab");
+	//$.('.nav-tabs a[href="#preview"]').tab('show');
+}
+
+//meta
+function attachMeta(fileId){
+  console.log("attachMeta");
+  var scope = "global";
+  boxClient.metadata.createFileMetadata({
+  	fileId: fileId,
+	scope: scope,
+	body: {
+	  userId: userId
+	},
+	templateKey: "properties"
+  });
+  console.log("attachMeta 2x");
+}
+
+
 
 
